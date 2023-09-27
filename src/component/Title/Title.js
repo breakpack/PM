03f2b89bb61557e3e059475a8a3809ab156b04c7
breakpack <198799879a@gmail.com>
2023-09-27 15:03:04 +0900

    first commit

---
+++ src/component/Title/Title.js
@@ -0,0 +1,13 @@
+import './Title.css';
+
+function App() {
+  return (
+    <div className="wapper">
+      <div className='Title'>Hi! This is PM</div>
+      <div className='Bar'></div>      
+      <div className='by'>by breakpack</div>   
+    </div>
+  );
+}
+
+export default App;
