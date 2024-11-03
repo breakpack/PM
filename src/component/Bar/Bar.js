f9d77f378c5fb7c413ab13af9a82c2dc8de240db
breakpack <198799879a@gmail.com>
2024-11-03 19:28:18 +0900

    백업

--- src/component/Bar/Bar.js
+++ src/component/Bar/Bar.js
@@ -1,10 +1,10 @@
-import * as s from '../CPstyle/Bar.tsx';
+import * as s from "../CPstyle/Bar.tsx";
 
 function Bar() {
   return (
     <s.bottom_Bar>
-        <s.Linktag to='/login'>Login</s.Linktag>
-        <s.Linktag to='/register'>Register</s.Linktag>
+      <s.Linktag to="/login">Login</s.Linktag>
+      <s.Linktag to="/register">Register</s.Linktag>
     </s.bottom_Bar>
   );
 }
